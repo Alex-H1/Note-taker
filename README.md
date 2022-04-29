e3131c713454ced2de01b59a573175f92edf51d6
alex hernandez <alexanderohernandez21@gmail.com>
2022-04-28 21:15:16 -0700

    fixed push

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Note-taker
+
+## Description 
+
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('./helper/uuid');
 const notes = require('./db/db.json');
+
+
 // const { text } = require('express');
 // const { title } = require('process');
 // const { randomUUID } = require('crypto');
@@ -10,6 +12,9 @@ const notes = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -27,78 +32,78 @@ app.get('/notes', (req, res) =>
 
 // get request for notes
 app.get('/api/notes',(req,res)=>{
-  consol.log(notes);
+  console.log(notes);
   res.json(notes);
   console.info(`${req.method} request recieved to get notes`)
 });
 
-// app.post('/api/notes',(req,res)=>{
-//   console.info(`${req.method} request recieved to add a note`);
-  
-//   const { title , text } = req.body;
-//   if(title && text){
-//     const newNote = {
-//       title,
-//       text,
-//       note_id: uuid(),
-//     };
-//     const reviewNote = JSON.stringify(newNote);
-  
-//   fs.writeFile(`./db/${newNote}.json`,reviewNote, (err)=>
-//     err ? console.error(err) : console.log `Review for ${newNote} has been written to JSON file`  
-//   );
-
-//   const response ={
-//     status: 'success',
-//     body: newNote,
-//   };
-//   console.log(response);
-//   res.json(response)
-//   }else{
-//     res.json('error in posting note');
-//   }
-// })
-
-app.post('/api/notes',(req, res)=>{
+app.post('/api/notes',(req,res)=>{
   console.info(`${req.method} request recieved to add a note`);
-
-  const {title , text} = req.body;
+  
+  const { title , text } = req.body;
   if(title && text){
-    const newNote ={
+    const newNote = {
       title,
       text,
       note_id: uuid(),
     };
-    const parsedNotes = JSON.parse(data);
+    const reviewNote = JSON.stringify(newNote);
+  
+  fs.appendFile(`./db/db.json`,reviewNote, (err)=>
+    err ? console.error(err) : console.log `Review for ${newNote} has been written to JSON file`  
+  );
 
-    parsedNotes.push(newNote);
-    notes = newNote;
-
-    // fs.writeFile('./db/db.json','utf-8',JSON.stringify(parsedNotes),
-    // (writeErr)=>writeErr? console.error(writeErr) : console.info('succesfullt updated notes')
-    // );
-    fs.readFile('./db/db.json', 'utf-8',(err,data)=>{
-      if(err){
-        console.error(err);
-      }else{
-        const parsedNotes = JSON.parse(data);
-
-        parsedNotes.push(newNote);
-        notes = newNote;
-
-        fs.writeFile('./db/db.json',JSON.stringify(parsedNotes),
-        (writeErr)=>writeErr? console.error(writeErr) : console.info('succesfully updated ntoes!')
-        );
-      };
-    });
-    const response ={
-      status: 'success',
-      body: newNote
-    };
-    console.log(response);
-    res.json(response)  
+  const response ={
+    status: 'success',
+    body: newNote,
+  };
+  console.log(response);
+  res.json(response)
   }else{
-    res.json('Error in posting note')
+    res.json('error in posting note');
   }
+})
+
+// app.post('/api/notes', function(req, res){
+//   console.info(`${req.method} request recieved to add a note`);
+
+//   const { title , text} = req.body;
+//   if(title && text){
+//     const newNote ={
+//       title,
+//       text,
+//       note_id: uuid(),
+//     };
+//     const parsedNotes = JSON.parse(data);
+
+//     parsedNotes.push(newNote);
+//     notes = newNote;
+
+//     // fs.writeFile('./db/db.json','utf-8',JSON.stringify(parsedNotes),
+//     // (writeErr)=>writeErr? console.error(writeErr) : console.info('succesfullt updated notes')
+//     // );
+//     fs.readFile('./db/db.json', 'utf-8',(err,data)=>{
+//       if(err){
+//         console.error(err);
+//       }else{
+//         const parsedNotes = JSON.parse(data);
+
+//         parsedNotes.push(newNote);
+//         notes = newNote;
+
+//         fs.writeFile('./db/db.json',JSON.stringify(parsedNotes),
+//         (writeErr)=>writeErr? console.error(writeErr) : console.info('succesfully updated ntoes!')
+//         );
+//       };
+//     });
+//     const response ={
+//       status: 'success',
+//       body: newNote
+//     };
+//     console.log(response);
+//     res.json(response)  
+//   }else{
+//     res.json('Error in posting note')
+//   }
  
-});
+// });
